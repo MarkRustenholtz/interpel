ab1d81e47a89dd7ceb10c238c39a1019e8333bcf
MarkRustenholtz <markrusten@gmail.com>
2025-10-04 20:15:00 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'pv-gendarmerie-v1';
+const URLS_TO_CACHE = [
+  './',
+  './pv-esi-app.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(URLS_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => { if (key !== CACHE_NAME) return caches.delete(key); }))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).then(fetchRes => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, fetchRes.clone());
+          return fetchRes;
+        });
+      }).catch(() => caches.match('./pv-esi-app.html'));
+    })
+  );
+});
